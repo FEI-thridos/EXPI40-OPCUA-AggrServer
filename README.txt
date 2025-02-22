15bf2390a68dabea69b9008056a112ac25661b74
Martin Pajpach <martin.pajpach@chropynska.sk>
2025-02-22 14:09:56 +0100

    Uploading files

---
+++ README.txt
@@ -0,0 +1,2 @@
+Pri pridávaní nového OPC UA serveru na agregáciu je tento nutné pridať 
+ako nový <ConfiguredEndpoint> v Quickstarts.AggregationServer.Config.xml.
---
+++ StaticVars.cs
@@ -0,0 +1,15 @@
+﻿using Opc.Ua;
+using Opc.Ua.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregationServer
+{
+    internal static class StaticVars
+    {
+        internal static Dictionary<ConfiguredEndpoint, AggregationClientSession> _activeSessions = new Dictionary<ConfiguredEndpoint, AggregationClientSession>();
+    }
+}
